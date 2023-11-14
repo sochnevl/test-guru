761348d67bde4889db04ea4a8826df9f54a0d632
Alex <alexiamxsmart@gmail.com>
2023-11-14 19:28:41 +0300

    fix: added tests questions count

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
 
   def question_header(question)
     question.persisted? ? "Edit #{question.test.title} Question" : "Create New #{@test.title} Question"
-  ends
+  end
 
   def github_url(author, repo)
     link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: "_blank",  rel: 'nofollow', rel: 'noopener'
