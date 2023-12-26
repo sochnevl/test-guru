a2ed2a4484a6f4ebc109e728c6c03154ca2b564e
Alex <alexiamxsmart@gmail.com>
2023-12-26 22:40:39 +0300

    add: some localization (screencast)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   def after_sign_in_path_for(resource)
     if current_user.admin?
       admin_tests_path
@@ -11,5 +13,15 @@ class ApplicationController < ActionController::Base
 
   def admin?
     self.type == 'Admin'
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
