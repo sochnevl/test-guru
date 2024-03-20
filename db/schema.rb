0553f5c228042cde991abe8caa5ba5796fcc94b1
Alex <alexiamxsmart@gmail.com>
2024-03-20 22:35:50 +0300

    fix(переменная окружения PROD_DATABASE_URL): исправлена проблема локального запуска

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2024_01_13_183819) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "question_id", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(version: 2024_01_13_183819) do
   end
 
   create_table "gists", force: :cascade do |t|
-    t.integer "question_id", null: false
-    t.integer "author_id", null: false
+    t.bigint "question_id", null: false
+    t.bigint "author_id", null: false
     t.string "url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 2024_01_13_183819) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "test_id", null: false
+    t.bigint "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
@@ -47,12 +50,12 @@ ActiveRecord::Schema.define(version: 2024_01_13_183819) do
 
   create_table "test_passings", force: :cascade do |t|
     t.string "status"
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "correct_questions_counter", default: 0, null: false
-    t.integer "current_question_id"
+    t.bigint "current_question_id"
     t.index ["current_question_id"], name: "index_test_passings_on_current_question_id"
     t.index ["test_id"], name: "index_test_passings_on_test_id"
     t.index ["user_id"], name: "index_test_passings_on_user_id"
@@ -61,8 +64,8 @@ ActiveRecord::Schema.define(version: 2024_01_13_183819) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
-    t.integer "author_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
